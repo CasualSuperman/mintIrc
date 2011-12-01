901e113a449c3c9342393d6d53e85a1a09be95e1
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-01 14:27:52 -0500

    Made _update a private method.

--- js/metro.js
+++ js/metro.js
@@ -1,39 +1,42 @@
 Metro = {
-    ui:{
-        accents: {
-            blue: "#00AEDB",
-            green: "#00B159",
-            orange: "#F37735",
-            pink: "#EC098C",
-            purple: "#7C4199",
-            red: "#D11141",
-            yellow: "#FFC425"
-        },
-        backgrounds: {
-            black: "#000",
-            white: "#FFF"
-        },
-
-        updateAccent: function(color) {
-            this.accentColor = color;
-            this._update();
-        },
-        updateBackground: function(color) {
-            this.backgroundColor = color;
-            this._update();
-        },
-        _update: function() {
+    ui: (function() {
+        function _update() {
             $("body").addClass("transition");
             less.refresh(true);
             StyleFix.process();
             setTimeout(function(){
                 $("body").removeClass("transition");
-            }, this.transitionLength);
-        },
-        config: {
-            accentColor: Metro.ui.accents.blue,
-            backgroundColor: Metro.ui.backgrounds.white,
-            transitionLength: 500
+            }, Metro.ui.config.transitionLength);
+        };
+
+        return {
+            accents: {
+                blue: "#00AEDB",
+                green: "#00B159",
+                orange: "#F37735",
+                pink: "#EC098C",
+                purple: "#7C4199",
+                red: "#D11141",
+                yellow: "#FFC425"
+            },
+            backgrounds: {
+                black: "#000",
+                white: "#FFF"
+            },
+
+            updateAccent: function(color) {
+                this.accentColor = color;
+                _update();
+            },
+            updateBackground: function(color) {
+                this.backgroundColor = color;
+                _update();
+            },
+            config: {
+                accentColor: Metro.ui.accents.blue,
+                backgroundColor: Metro.ui.backgrounds.white,
+                transitionLength: 500
+            }
         }
-    }
+    })()
 };

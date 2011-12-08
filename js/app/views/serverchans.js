7512b7748549fd8a55051ca52eab6658cd8c8bbb
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-08 03:31:05 -0500

    fixed function references.

--- js/app/views/serverchans.js
+++ js/app/views/serverchans.js
@@ -7,12 +7,19 @@ var SrvChanView = Backbone.View.extend({
     },
     initialize: function() {
         // Expects model to be a ChanList
-        this.model.bind('add', this.render, this);
+        this.model.bind('add', this.add, this);
+        this.model.bind('remove', this.remove, this);
     },
     render: function() {
         this.el.appendChild(new ChanNameView({model: this.model.last()}).el);
     },
     switchRooms: function(e) {
+        console.log(e);
+    },
+    add: function(e) {
+        console.log(e);
+    },
+    remove: function(e) {
         console.log(e);
     }
 });

f21967a798a494038ee6cbf2bd283654b6c27d67
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-16 11:50:38 -0500

    Wrote render for main app.

--- js/app/views/desktop/Irc.js
+++ js/app/views/desktop/Irc.js
@@ -2,18 +2,64 @@ var Irc = Backbone.View.extend({
     tagName: "body",
     classname: "",
 
-    serverList: null,
+    serverList: new ServerList,
+    serverViewList: [],
+
+    ActiveServer: null;
 
     initialize: function() {
-        this.el.appendChild(new MenuView({}));
+        this.serverList.forEach(function(elem) {
+            this.serverViewList.push(new ServerView({model: elem}));
+        }, this);
         this.render();
     },
 
     render: function() {
         this.el = this.make(this.tagName, {class: this.className});
-        serverList.forEach(function(elem) {
-            this.el.appendChild(new ServerListItemView({model: elem}).render().el);
+
+        _.each(serverViewList, function(view) {
+            if (view.active) {
+                this.ActiveServer = view;
+            }
         }, this);
+        // Hide node to prevent reflow.
+        this.el.style.display = "none";
+        // Remove all children.
+        while (this.el.hasChildNodes())
+            this.el.removeChild(this.el.lastChild);
+
+        // Add the active views.
+        var header = this.make("header", {class: "connection-list"});
+        // Add the servers
+        var servers = this.make("ul", {class: "servers"});
+        _.each(serverViewList, function(elem) {
+            servers.appendChild(elem.NameElement);
+        }, this);
+        servers.appendChild(this.make("li", {class: "server nonitem"}, "+"));
+        // Add the servers
+        header.appendChild(servers);
+        // List of chans
+        var chans = this.make("ul", {class: "chans"});
+        _.each(ActiveServer.ChanView, function(elem) {
+            chans.appendChild(elem.NameElement);
+        }, this);
+        chans.appendChild(this.make("li", {class: "chan nonitem"}, "+"));
+        // Add the chans
+        header.appendChild(chans);
+        // Add the gradient
+        header.appendChild(this.make("div", {class: "gradient"}));
+
+        var chat = this.make("div", {class: "chat"});
+        var container = this.make("div", {class: "container"});
+        container.appendChild(ActiveServer.ActiveChan.Messages);
+        chat.appendChild(container);
+        chat.appendChild(this.make("input", {class: "chat"}));
+
+        this.el.appendChild(chat);
+
+        // Restore the node.
+        this.el.style.display = "";
+
         return this;
     },
 });

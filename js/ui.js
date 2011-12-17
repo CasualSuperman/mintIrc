41d90df6757b37b03041b6026f14441dd8c85e22
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-17 18:10:38 -0500

    Removing old code.

--- js/ui.js
+++
@@ -1,9 +0,0 @@
-(function (){
-    window.onload = function() {
-        resize();
-    }
-    function resize() {
-        var offset = document.getElementsByTagName("header")[0].clientHeight;
-        document.getElementById("chat").style.marginTop = (offset) + "px";
-    }
-})();

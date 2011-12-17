214f8cacd973f31e7274d36203bcf04c315a907f
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-17 18:12:26 -0500

    I should have known to do this.

--- js/evil.js
+++ js/evil.js
@@ -1,4 +1,4 @@
-HTMLElement.prototype.prependChild = function(elem) {
+HTMLElement.prototype.prependChild = HTMLElement.prototype.prependChild || function(elem) {
     if (this.firstChild) {
         this.insertBefore(elem, this.firstChild);
     } else {

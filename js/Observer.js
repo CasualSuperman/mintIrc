1f8b1835c8e18ad6827719d3b240696fd30c7bb6
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-17 16:26:42 -0500

    Decided to use underscore.js

---
+++ js/Observer.js
@@ -0,0 +1,54 @@
+_.mixin({
+    on: function(obj, event, callback) {
+        // Define a global Observer
+        if(this.isString(obj)) {
+            callback = event;
+            event = obj;
+            obj = this;
+        }
+        if(this.isUndefined(obj._events)) {
+            obj._events = {};
+        }
+        if (!(event in obj._events)) {
+            obj._events[event] = [];
+        }
+        obj._events[event].push(callback);
+        return this;
+    },
+    once: function(obj, event, callback) {
+        if(this.isString(obj)) {
+            callback = event;
+            event = obj;
+            obj = this;
+        }
+        var removeEvent = function() {
+            _.removeEvent(obj, event);
+        };
+        callback = _.compose(removeEvent, callback);
+        this.on(obj, event, callback);
+    },
+    emit: function(obj, event, args){
+        if(this.isString(obj)) {
+            callback = event;
+            event = obj;
+            obj = this;
+        }
+        if(this.isUndefined(obj._events))
+            return;
+        if (event in obj._events) {
+            var events = obj._events[event].concat();
+            for (var i = 0, ii = events.length; i &lt; ii; i++)
+                events[i].apply(obj, args === undefined ? [] : args);
+        }
+        return this;
+    },
+    removeEvent: function(obj, event) {
+        if(this.isString(obj)) {
+            event = obj;
+            obj = this;
+        }
+        if(this.isUndefined(obj._events))
+            return;
+        delete obj._events[event];
+    }
+});

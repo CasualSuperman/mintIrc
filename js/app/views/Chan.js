ae31e0687bd55f4a6980156168997f6302ed4838
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-19 20:42:46 -0500

    Chan does things with events! It's great!

--- js/app/views/Chan.js
+++ js/app/views/Chan.js
@@ -1,24 +1,54 @@
 var ChanView = function(chan) {
-    this.el = {
+    var elements = {
+        // The Menu item.
         li: util.create("li", ["chan"], chan.name),
-        msgs: util.create("div", ["chat"], [
-            util.create("div", ["log_container"]),
-            util.create("input")]);
-        )
+        // The "body" view.
+        messages: util.create("div", ["chat"], [
+                    util.create("div", ["log_container"], 
+                        util.create("table", ["log"], util.create("tbody"))
+                    ),
+                    util.create("input")])
     };
+    // Get a reference to our table for ease of use.
+    var log = elements.messages.getElementsByTagName("tbody")[0];
 
-    this.log = this.el.msgs.getElementsByClassName("log_container")[0];
-    this.log.appendChild(util.create("table", ["log"], util.create("tbody")));
-    this.log = this.log.firstChild;
+    // Append all the already made messages.
+    _(chan.messages).each(function(msg) {
+        log.appendChild(new MessageView(msg).el);
+    });
 
     _(chan).on("add-msgs", (function(context) {
         return function(premsgs, postmsgs) {
             _(premsgs).each(function(msg) {
-                this.log.prependChild(new MessageView(msg).el);
+                log.prependChild(new MessageView(msg).el);
             }, context);
             _(postmsgs).each(function(msg) {
-                this.log.appendChild(new MessageView(msg).el);
+                log.appendChild(new MessageView(msg).el);
             }, context);
         };
     }(this)));
+
+    _(this).on("deactivate", (function(context) {
+        return function() {
+            elements.li.className = elements.li.className.replace(/\s+active|active\s+|active/, "");
+            context.active = false;
+        };
+    }(this)));
+
+    elements.li.onclick = (function(context) {
+        return function() {
+            _(context).emit("activate");
+        };
+    }(this));
+
+    _(this).on("activate", (function(context) {
+        return function() {
+            elements.li.className +=  " active";
+            context.active = true;
+        };
+    }(this)));
+
+    this.el = elements;
+    this.active = false;
+    return this;
 }
